760af151483ac8dcebdb89bbfb2c293067929638
prasojopxl <prasojodesigner@gmail.com>
2021-06-21 12:06:43 +0700

    layout home v1

---
+++ components/layouthome/layoutHome.jsx
@@ -0,0 +1,104 @@
+import Head from "next/head";
+import styles from "./layouthome.module.scss";
+import Image from "next/image";
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import { apiUrl } from "../../config/variable";
+
+
+export default function LayoutHome(props) {
+    const [menu, setMenu] = useState([]);
+    const [lalestPost, setLatestPost]= useState([]);
+    const [logo, setLogo]= useState([]);
+
+    const getMenu = () => {
+        axios.get(`${apiUrl}/menus`)
+        .then((res)=> {
+            setMenu(res.data)
+        })
+    }
+
+    const getGeneral = () => {
+        axios.get(`${apiUrl}/general`)
+        .then((res)=>{
+            setLogo(res.data.logo.url)
+        })
+    }
+
+    const getLatestPost = () => {
+        axios.get(`${apiUrl}/posts?_limit=3&_sort=updated_at`)
+        .then((res)=> {
+            setLatestPost(res.data)
+        })
+    }
+
+    useEffect(()=>{
+        getMenu();
+        getLatestPost();
+        getGeneral();
+    },[])
+    return (
+        <React.Fragment>
+            <Head>
+                <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+                <title>{props.title}</title>
+                <meta name="description" content="Description page here"/>
+                <meta name="keywords" content="Frontend, Website, HTML, CSS"/>
+                <meta name="author" content="Developer"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+                <link rel="shortcut icon" href="fav.svg"/>                
+            </Head>         
+
+            <div className={styles.headerweb}>
+                <div className={styles.contents}>
+                    <div className={styles.left}>                        
+                        <a href="#" className={styles.mainlogo}>
+                            logo                          
+                        </a>
+                    </div>
+                    <div className={styles.right}>
+                        <ul className={styles.mainmenu}>
+                            {menu.map((item,i)=> {
+                                return (
+                                    <li key={item.id}><a href="#">{item.title}</a></li>
+                                )
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>            
+
+            <div className={styles.bodyweb}>
+                <div className={styles.wrppages}>
+                    <div className={styles.contents}>
+                        <div className="fullbaner">
+                            <div className="imgwrp"><img src="https://dummyimage.com/1200x200/d1cfd1/a1a2a8&text=Full+baner+Ads" alt=""/></div>
+                        </div>
+                        <div className={styles.categoriesHome}>
+                            <ul>
+                                <li><a href="#">Smartphone</a></li>
+                                <li><a href="#">Smart TV</a></li>
+                                <li><a href="#">Jam Tangan</a></li>
+                                <li><a href="#">Lain-Lain</a></li>
+                            </ul>
+                        </div>
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <div className={styles.boxwhite}>
+                                    {props.children}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>            
+            
+            <div className={styles.footerweb}>
+                <div className={styles.contents}>
+                    Footer
+                </div>
+            </div>
+
+        </React.Fragment>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@ import Head from 'next/head'
 import React, {useState, useEffect} from 'react';
 import Image from "next/image";
 import styles from "./Pages.module.scss";
-import Layout from "../components/layout/layout";
+import LayoutHome from '../components/layouthome/layoutHome';
 import axios from 'axios';
 import { apiUrl } from '../config/variable';
 
 export default function Home() {
   const [post, setPost] = useState([]);
+  const [banerTop, setBanerTop]= useState([]);
+  const [subBanerTop, setSubBanerTop] = useState([]);
 
   const getPost = () => {
     axios.get(`${apiUrl}/posts`)
@@ -15,13 +17,78 @@ export default function Home() {
       setPost(res.data);
     })
   }
+
+  const getSubBanerTop = () => {
+    axios.get(`${apiUrl}/products?_start=2&_sort=updated_at:DESC&_limit=3`)
+    .then((res)=> {
+      setSubBanerTop(res.data)
+    })
+  }
+
+  const getBanerTop = () => {
+    axios.get(`${apiUrl}/products?_sort=updated_at:DESC&_limit=1`)
+    .then((res)=> {
+      setBanerTop(res.data)
+    })
+  }
+
   useEffect(()=> {
-    getPost()
+    getPost();
+    getBanerTop();
+    getSubBanerTop();
   },[])
 
   return (
     <React.Fragment>
-      <Layout title="androidponsel.com">
+      <LayoutHome title="androidponsel.com">
+          <div className={styles.banerHome}>
+            <div className={styles.left}>
+              {banerTop.map((item,i)=> {
+                  return (
+                    <div key={item.id}>
+                      <div className={styles.itemContent}>
+                        <div className="row">
+                          <div className="col-lg-6">
+                            <div className={styles.vcenter}>
+                              <Image
+                                src={item.product_image[0].url}
+                                alt={item.title}
+                                width={item.product_image[0].width}
+                                height={item.product_image[0].height}
+                                />
+                            </div>
+                          </div>
+                          <div className="col-lg-6">
+                            <div className="txtleft">
+                              <h2><a href="#">{item.title}</a></h2>
+                              {(item.updated_at).substr(2,2)}-{(item.updated_at).substr(5,2)}-{(item.updated_at).substr(0,4)}
+                              <p>{(item.SEO.description_tag).substr(0,200)}...</p>
+                              <a href="#">Lebih Lanjut</a>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  )
+                })
+              }
+            </div>
+            <div className={styles.right, styles.banerHomeRight}>
+              {subBanerTop.map((item,i)=> {
+                return (
+                    <div className={styles.itembaner} key={item.id}>
+                      <div className={styles.imgwrp}>
+                        <Image src={item.product_image[0].url} width={item.product_image[0].width} height={item.product_image[0].height} alt={item.title}/>
+                      </div>
+                      <div className={styles.contentSub}>
+                        <h5><a href="#">{item.title}</a></h5>
+                        <span>{(item.updated_at).substr(8,2)}-{(item.updated_at).substr(5,2)}-{(item.updated_at).substr(0,4)}</span>
+                      </div>
+                    </div>
+                )
+              })}
+            </div>
+          </div>
           <div className={styles.wrpPosts}>
             <div className="row">
               {post.map((item, i)=> {
@@ -39,7 +106,7 @@ export default function Home() {
               })}
             </div>
           </div>
-      </Layout>
+      </LayoutHome>
     </React.Fragment>
   )
 }
