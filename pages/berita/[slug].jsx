9e7d6e62e1cf1b838e4b552b689bf386fa48c080
prasojopxl <prasojodesigner@gmail.com>
2021-07-16 19:03:38 +0700

    pages/berita/[slug] belum selesai

--- pages/berita/[slug].jsx
+++ pages/berita/[slug].jsx
@@ -1,33 +1,48 @@
-import { useRouter } from "next/router"
-import { apiUrl } from "../../config/variable";
+// import { useRouter } from "next/router"
+// import { apiUrl } from "../../config/variable";
 import LayoutBerita from "../../layout/layoutberita/LayoutBerita";
 
-export async function getServerSideProps(context) {
-    const slug = context.query.slug;
-    try {
-        const resDataBerita = await fetch(`${apiUrl}/posts/${slug}`)
-        const dataBerita = await resDataBerita.json();
-        return {
-            props: {
-                dataBerita
-            }
-        }    
-    } catch (error) {
-        return {error};
-    }
-}
-
-export default function DetailBerita({dataBerita}) {
-    const router = useRouter();
-    const {slug} = router.query;
-    console.log(dataBerita)
+export default function DetailBerita() {
     return (
         <LayoutBerita>
         <div>
-            <h4>{dataBerita.title && dataBerita.title}</h4>
-            {dataBerita.content && dataBerita.content}
+            Halo
         </div>
         </LayoutBerita>
     )
 }
+
+// export async function getStaticPaths() {
+//     return {
+//       paths: [
+//         { params: { ... } } // See the "paths" section below
+//       ],
+//       fallback: true or false // See the "fallback" section below
+//     };
+//   }
+
+// Lihat di sini: https://www.youtube.com/watch?v=628ZBS6dWSM&list=PLLhpK5IKtgqnexMqh_UdR9OKV_nD_huPR&index=13
+export async function getStaticPaths() {
+    return {
+        paths: [
+            {params: {slug:"p01" }}
+        ],
+        fallback:false
+    };
+}
+
+// export async function getServerSideProps(context) {
+//     const slug = context.query.slug;
+//     try {
+//         const resDataBerita = await fetch(`${apiUrl}/posts/${slug}`)
+//         const dataBerita = await resDataBerita.json();
+//         return {
+//             props: {
+//                 dataBerita
+//             }
+//         }    
+//     } catch (error) {
+//         return {error};
+//     }
+// }
 
