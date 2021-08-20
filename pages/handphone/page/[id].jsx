ffe045e250ef1715aa209a505652f8e708be05a6
prasojopxl <prasojodesigner@gmail.com>
2021-08-20 17:45:51 +0700

    paging v1

--- pages/handphone/page/[id].jsx
+++ pages/handphone/page/[id].jsx
@@ -1,18 +1,22 @@
+import { fetchData } from "../../../config/data";
 import { apiUrl } from "../../../config/variable";
+import Layout from "../../../layout";
 
 export default function Page(props) {
     return (
-        <div>
-            asd
-        </div>
+        <Layout
+            dataSEO={props.dataSEO.seo}
+            dataMainMenu={props.getMenu}
+            dataBrands={props.getTopBrands}
+        >
+            dasd
+        </Layout>
+
     )
 }
 
 export async function getStaticPaths() {
-    const res = await fetch(`${apiUrl}/products?category=1`);
-    const posts = await res.json();
-
-
+    const posts = await fetchData("/products?category=1");
     const paths = posts.map((item, i) => ({
         params: {
             id: `${item.id}`
@@ -20,15 +24,27 @@ export async function getStaticPaths() {
     }))
     return {
         paths,
-        fallback: true
+        fallback: false
     }
 }
 
 export async function getStaticProps({ params }) {
+    const dataSEO = await fetchData("/general");
+    const getMenu = await fetchData("/menus?_sort=order");
+    const getTopBrands = await fetchData("/brands?_top_brand=true");
+    if (!dataSEO || !getMenu || !getTopBrands) {
+        return {
+            notFound: true,
+        };
+    }
     return {
         props: {
-            params
-        }
+            params,
+            getMenu,
+            getTopBrands,
+            dataSEO,
+        },
+        revalidate: 3
     }
 }
 
