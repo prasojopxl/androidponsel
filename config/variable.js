816d638d6c1fc6053c100131a0c91de895e569fd
prasojopxl <prasojodesigner@gmail.com>
2021-08-27 02:22:37 +0700

    change variable

--- config/variable.js
+++ config/variable.js
@@ -1,5 +1,5 @@
 export const apiUrl = `https://papiandro.stagingaja.com`;
-export const baseUrl = `https://android-omega.vercel.app/`;
+export const baseUrl = `https://www.androidponsel.com/device/`;
 export const apiUrlBlog = `https://www.androidponsel.com/wp-json/wp/v2/posts?`;
 export const apiUrlApp = `https://www.androidponsel.com/download/wp-json/wp/v2/posts?`;
 export const totalItem = 8;
