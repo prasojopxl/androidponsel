939ba1aa65d9d36c502abf443f056284baed3d96
Sapto Prasojo <prasojopxl@users.noreply.github.com>
2021-09-03 19:37:34 +0700

    add folder config

---
+++ config/variable.js
@@ -0,0 +1,5 @@
+export const apiUrl = `https://papiandro.stagingaja.com`;
+export const baseUrl = `http://localhost:3000/`;
+export const apiUrlBlog = `https://www.androidponsel.com/wp-json/wp/v2/posts?`;
+export const apiUrlApp = `https://www.androidponsel.com/download/wp-json/wp/v2/posts?`;
+export const totalItem = 8;
