2849808abeb93a287140cebde3b9df4acc84466c
prasojopxl <prasojodesigner@gmail.com>
2021-09-13 19:10:42 +0700

    search

--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -14,11 +14,19 @@ export default function Header(props) {
     const { mainMenu, brands, searchProd } = props;
     const [inputVal, setInputVal] = useState("");
     const [people, setPeople] = useState([]);
-    const [dataProd, setDataProd] = useState([])
+    const [showSearch, setShowSearch] = useState(false)
+    const [showResult, setShowResult] = useState(false)
     const getDataProd = () => {
         axios.get(`${apiUrl}/products`).then(res => {
             setPeople(res.data)
         })
+    }
+    const handleChange = e => {
+        setInputVal(e.target.value);
+        e.target.value === "" ? setShowSearch(false) : setShowSearch(true)
+    };
+    const resultSearch = () => {
+        showResult === false ? setShowResult(true) : setShowResult(false)
     }
     useEffect(() => {
         getDataProd();
@@ -51,10 +59,46 @@ export default function Header(props) {
                         </ul>
                     </div>
                     <div className={styles.right}>
-                        <FontAwesomeIcon icon={faSearch} />
+                        <FontAwesomeIcon icon={faSearch} onClick={resultSearch} style={{ cursor: "pointer" }} />
                     </div>
                 </div>
             </div>
+            {
+                showResult &&
+                <>
+                    <div className={styles.inputSearch}>
+                        <span>Cari Handphone</span>
+                        <input
+                            type="text"
+                            value={inputVal}
+                            onChange={handleChange}
+                            placeholder="search..."
+                        />
+                    </div>
+                    <div className={styles.searchResult}>
+                        {showSearch &&
+                            people.filter((person) => {
+                                if (inputVal === "") {
+                                    return person;
+                                } else if (
+                                    person.title
+                                        .toLowerCase()
+                                        .includes(inputVal.toLowerCase())
+                                ) {
+                                    return person;
+                                }
+                            })
+                                .map((item, i) => {
+                                    return (
+                                        <div className={styles.itemResult} key={item.id}>
+                                            <a href={`${baseUrl}handphone/${item.slug}`}>{item.title}</a>
+                                        </div>
+                                    );
+                                })}
+                    </div>
+
+                </>
+            }
             <div className={styles.TopBrands}>
                 <label>Merk:</label>
                 <ul>
@@ -67,35 +111,6 @@ export default function Header(props) {
                     }
                 </ul>
             </div>
-
-            <input
-                type="text"
-                value={inputVal}
-                onChange={(e) => {
-                    setInputVal(e.target.value);
-                }}
-                placeholder="search..."
-            />
-            {people
-                .filter((person) => {
-                    if (inputVal === "") {
-                        return person;
-                    } else if (
-                        person.title
-                            .toLowerCase()
-                            .includes(inputVal.toLowerCase())
-                    ) {
-                        return person;
-                    }
-                })
-                .map((item, i) => {
-                    return (
-                        <div key={item.id}>
-                            <a href={item.slug}>{item.title}</a>
-                        </div>
-                    );
-                })}
-
         </Fragment >
     );
 }
