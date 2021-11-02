96b322b12a56c20dab50855a4920993e4d2686cc
root <prasojodesigner@gmail.com>
2021-11-02 22:14:54 +0000

    rev live

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 module.exports = {
     future: { webpack5: true },
+    basePath: '/device',
     images: {
         domains: [
             "images.unsplash.com",
             "localhost",
             "www.androidponsel.com",
+            "api.androidponsel.com",
             "papiandro.stagingaja.com",
         ],
         // loader: "imgix",
