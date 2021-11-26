db5aab24257ffd32b522c0533ac3e9934fd7a371
prasojopxl <prasojodesigner@gmail.com>
2021-11-26 15:37:16 +0700

    rev ads

--- pages/index.js
+++ pages/index.js
@@ -97,11 +97,10 @@ export default function Home(props) {
     useEffect(() => {
         removeLocalProd();
         return getLocalProd();
-        var ads = document.getElementsByClassName("adsbygoogle").length;
-        for (var i = 0; i < ads; i++) {
-            try {
-                (adsbygoogle = window.adsbygoogle || []).push({});
-            } catch (e) {}
+        try {
+            (window.adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (err) {
+            console.log(err);
         }
     }, []);
     return (
@@ -156,7 +155,21 @@ export default function Home(props) {
                         property="og:url"
                         content="https://www.androidponsel.com/"
                     />
+                    <script
+                        async
+                        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4889404422926996"
+                        crossorigin="anonymous"
+                    ></script>
                 </Head>
+                <ins
+                    className="adsbygoogle"
+                    style={{ display: "block" }}
+                    data-ad-client="ca-pub-4889404422926996"
+                    data-ad-slot="2338723921"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"
+                ></ins>
+
                 {props.adsData1.Image_Banner ? (
                     <AdsBanner
                         urlImage={apiUrl + props.adsData1.Image_Banner.url}
