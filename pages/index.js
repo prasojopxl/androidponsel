9f700a85ba78df7cf62eb9c68257d54865836507
prasojopxl <prasojodesigner@gmail.com>
2021-12-05 06:24:33 +0700

    ads

--- pages/index.js
+++ pages/index.js
@@ -166,14 +166,14 @@ export default function Home(props) {
                         crossorigin="anonymous"
                     ></script>
                 </Head>
-                {/* <ins
+                <ins
                     className="adsbygoogle"
                     style={{ display: "block" }}
                     data-ad-client="ca-pub-4889404422926996"
                     data-ad-slot="2338723921"
                     data-ad-format="auto"
                     data-full-width-responsive="true"
-                ></ins> */}
+                ></ins>
 
                 {/* {props.adsData1.Image_Banner ? (
                     <AdsBanner
