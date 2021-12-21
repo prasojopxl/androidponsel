329c4d703e76f5140ab505d9add83fe57ab3f40b
prasojopxl <prasojodesigner@gmail.com>
2021-12-22 02:16:26 +0700

    rev component ads

--- components/loadAds/index.jsx
+++ components/loadAds/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function LoadAds() {
-    try {
-        if (typeof window !== "undefined") {
-            (window.adsbygoogle = window.adsbygoogle || []).push({});
-        }
-    } catch (error) {
-        console.log("adsense error", error.message);
+    // try {
+    //     if (typeof window !== "undefined") {
+    //         (window.adsbygoogle = window.adsbygoogle || []).push({});
+    //     }
+    // } catch (error) {
+    //     console.log("adsense error", error.message);
+    // }
+    var ads = document.getElementsByClassName("adsbygoogle").length;
+    for (var i = 0; i < ads; i++) {
+        try {
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        } catch (e) { }
     }
 }
